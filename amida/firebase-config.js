508d4e62c9f19e039f943c8dbcd2095db385247c
tp1012 <tp1012@gmail.com>
2026-06-08 21:21:14 +0900

    initial commit

---
+++ amida/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// ご自身の Firebase コンソールから取得した設定に書き換えてください。
+window.firebaseConfig = {
+  apiKey: "AIzaSyCUhuv_C64L8EVUr5n-xesQexa7egOx0qw",
+  authDomain: "amida-dca5b.firebaseapp.com",
+  projectId: "amida-dca5b",
+  storageBucket: "amida-dca5b.firebasestorage.app",
+  messagingSenderId: "934776647802",
+  appId: "1:934776647802:web:d9374e50e42452a46a8bc3",
+  measurementId: "G-7F7ZG4VZ1F"
+};
---
+++ sekokan/config.js
@@ -0,0 +1,17 @@
+/**
+ * Firebaseプロジェクトの設定
+ * * Firebase Console (https://console.firebase.google.com/) でプロジェクトを作成し、
+ * 「ウェブアプリ」を追加して表示される設定値を以下に貼り付けてください。
+ */
+window.firebaseConfig = {
+  apiKey: "AIzaSyBOHLNbdHfWo8VR6jTf4Y7WAQ_zLAhbq1g",
+  authDomain: "sekokan-745f5.firebaseapp.com",
+  projectId: "sekokan-745f5",
+  storageBucket: "sekokan-745f5.firebasestorage.app",
+  messagingSenderId: "29174497286",
+  appId: "1:29174497286:web:be5c4839d27dc40011c387",
+  measurementId: "G-ZKMWJQP4NB"
+};
+
+// アプリケーションを識別するためのID（必要に応じて変更してください）
+window.constructionAppId = 'construction-sim-v3';
